3098c67945d0d17af69ea466e3f331d9794a09d3
John Gathirua <john.gathirua@pathcarekenya.com>
2026-07-15 17:00:49 +0300

    Fix celery background worker

--- config/__init__.py
+++ config/__init__.py
@@ -16,3 +16,9 @@ try:
         template_context.BaseContext.__copy__ = _compat_basecontext_copy
 except Exception:
     pass
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
